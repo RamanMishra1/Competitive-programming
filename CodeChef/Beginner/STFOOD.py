0f875c0ebed11acc58692edbb2afa23cd4c7c1bc
RamanMishra1 <ramanmishra.rrps@gmail.com>
2020-01-25 23:49:54 +0530

    Python Programs

---
+++ CodeChef/Beginner/STFOOD.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    high=0
+    for i in range(int(input())):
+        s,p,v = map(int,input().split())
+        if(p>(s+1)):
+            m=int(p/(s+1))*v
+            if m > high:
+                high = m
+    print(high)
